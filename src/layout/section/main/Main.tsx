0f326b2a0bfb67168aa035a16101717f5f673235
Anton Brashkov <spone161@gmail.com>
2023-08-14 20:42:38 +0300

    add main

---
+++ src/layout/section/main/Main.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from "styled-components";
+import  photo from '../../../assets/images/photo.webp'
+import {FlexWrapper} from "../../../components/FlexWrapper";
+export const Main = () => {
+    return (
+        <StyledMain>
+           <FlexWrapper align={'center'} justify={'space-around'}>
+               <div>
+                   <span>Hi There</span>
+                   <Name>I am Anton Barashkov</Name>
+                   <MainTitle>A Web Developer.</MainTitle>
+               </div>
+
+               <Photo src={photo} alt='Photo author'/>
+           </FlexWrapper>
+        </StyledMain>
+    );
+};
+
+const StyledMain = styled.div`
+  max-height: 100vh;
+  background-color: #f5b0b0;
+`
+
+const Photo = styled.img`
+  width: 350px;
+  height: 430px;
+  object-fit: cover;
+`
+
+const MainTitle = styled.h1`
+  
+`
+const Name = styled.h2`
+
+`
