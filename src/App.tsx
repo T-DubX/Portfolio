06f8f8e50ec054250492e2f45ce6ef1b9cafd639
Anton Brashkov <spone161@gmail.com>
2023-08-16 11:27:50 +0300

    add contact

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import {Main} from "./layout/section/main/Main";
 import {Skills} from "./layout/section/skills/Skills";
 import {Works} from "./layout/section/works/Works";
 import {Testimony} from "./layout/section/testimony/Testimony";
+import {Contact} from "./layout/section/contact/Contact";
+
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
             <Skills/>
             <Works/>
             <Testimony/>
+            <Contact/>
         </div>
     );
 }
