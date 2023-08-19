f3188d56e1d97679322ace17918ff49b2e23220d
Anton Brashkov <spone161@gmail.com>
2023-08-19 22:13:07 +0300

    add theme

--- src/styles/GlobalStyled.tsx
+++ src/styles/GlobalStyled.tsx
@@ -1,4 +1,5 @@
 import {createGlobalStyle} from "styled-components";
+import {theme} from "./Theme";
 
 export const GlobalStyle = createGlobalStyle`
   *,
@@ -16,6 +17,7 @@ export const GlobalStyle = createGlobalStyle`
     sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    color: ${theme.colors.font};
   }
   
   a {
@@ -29,6 +31,14 @@ export const GlobalStyle = createGlobalStyle`
   button {
     background-color: unset;
     border: unset;
+  }
+  
+  section:nth-of-type(odd) {
+    background-color: ${theme.colors.primaryBg};
+  }
+
+  section:nth-of-type(even) {
+    background-color: ${theme.colors.secondaryBg};
   }
 
 `
