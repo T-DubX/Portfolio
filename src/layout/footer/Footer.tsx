80a2dacbaab8171adb95e54a075259a2b724a0d4
Anton Brashkov <spone161@gmail.com>
2023-08-20 15:21:09 +0300

    styling footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {theme} from "../../styles/Theme";
 
 export const Footer = () => {
     return (
@@ -37,18 +38,21 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-  min-height: 20vh;
-  background-color: #efbe99;
-
+  background-color: ${theme.colors.primaryBg};
+  padding: 40px 0;
 `
 
 const Name = styled.span`
-
+  font-family: 'Josefin Sans',sans-serif;
+  font-size: 22px;
+  font-weight: 700;
+  letter-spacing: 3px;
 `
 
 const SocialList = styled.ul`
   display: flex;
-  gap: 30px;
+  gap: 20px;
+  margin: 30px 0;
 `
 
 const SocialItem = styled.li`
@@ -56,8 +60,24 @@ const SocialItem = styled.li`
 `
 
 const SocialLink = styled.a`
-
+  background-color: rgba(255,255,255,0.1);
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  color: ${theme.colors.accent};
+  
+  &:hover {
+    color: ${theme.colors.primaryBg};
+    transform: translateY(-4px);
+  }
 `
 const Copyright = styled.small`
-
+  text-align: center;
+  font-size: 12px;
+  font-weight: 400;
+  opacity: .5;
 `
