3d650151ae63b21f065120d6dbfb2082b5f5b405
Anton Brashkov <spone161@gmail.com>
2023-08-20 15:01:31 +0300

    styling slogan

--- src/layout/section/slogan/Slogan.tsx
+++ src/layout/section/slogan/Slogan.tsx
@@ -3,15 +3,18 @@ import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Button} from "../../../components/Button";
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Container} from "../../../components/Container";
 
 export const Slogan = () => {
     return (
         <StyledSlogan>
-            <SectionTitle>I Am Available For Freelance</SectionTitle>
-            <FlexWrapper justify={'center'}>
-                <Button>Hire me</Button>
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper direction={'column'} align={'center'}>
+                    <SectionTitle>I Am Available For Freelance</SectionTitle>
 
+                    <Button>Hire me</Button>
+                </FlexWrapper>
+            </Container>
         </StyledSlogan>
     );
 };
@@ -19,6 +22,6 @@ export const Slogan = () => {
 const StyledSlogan = styled.section`
   min-height: 30vh;
   background-color: #d4f5cd;
-  
+
 `
 
